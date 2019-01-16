fa67092fbdcea907922ab5ef2ea4563d032b2f37
chenyue9713 <cy422311@gmail.com>
2019-01-15 16:21:43 -0800

    tuning cost function

--- mpc_to_line/src/MPC.cpp
+++ mpc_to_line/src/MPC.cpp
@@ -71,7 +71,7 @@ class FG_eval {
     }
 
     for(uint t = 1; t < N - 2; t++){
-      fg[0] += CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
+      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t],2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t],2);
     }
 
@@ -292,7 +292,7 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
 
 int main() {
   MPC mpc;
-  int iters = 50;
+  int iters = 60;
 
   Eigen::VectorXd ptsx(2);
   Eigen::VectorXd ptsy(2);
